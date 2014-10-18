84475915412b79a369606ba395128c1140d64323
nicolas silva <sovietfrance@gmail.com>
2014-10-18 18:42:45 -0300

    fix error - custom filter by Curo on ClaseAdmin

--- src/cursos/admin.py
+++ src/cursos/admin.py
@@ -1,34 +1,54 @@
 from django.contrib import admin
-from django.contrib.admin import SimpleListFilter
-from django.contrib.admin import ModelAdmin
+from django.contrib.admin import ModelAdmin, SimpleListFilter
 from .models import Area, Curso, Clase, Unidad
 
 class CursoFilter(SimpleListFilter):
     title = 'curso' # or use _('curso') for translated title
     parameter_name = 'curso'
+    
+    def lookups(self, request, model_admin):
+        cuursos = set([c.curso for c in model_admin.model.objects.all()])
+        return [(c.id, c.nombre) for c in cuursos]
+	
+	def queryset(self, request, queryset):
+		if self.value():
+			return queryset.filter(curso__id__exact=self.value())
+		else:
+			return queryset
+
+
+# admin.py
+class CountryFilter(SimpleListFilter):
+    title = 'curso' # or use _('country') for translated title
+    parameter_name = 'curso'
 
     def lookups(self, request, model_admin):
-        cursos = set([c.curso for c in model_admin.model.objects.all()])
-        return [(c.id, c.nombre) for c in cursos]
-        # You can also use hardcoded model name like "Curso" instead of 
+        countries = set([c.curso for c in model_admin.model.objects.all()])
+        return [(c.id, c.nombre) for c in countries]
+        # You can also use hardcoded model name like "Country" instead of 
         # "model_admin.model" if this is not direct foreign key filter
-	def queryset(self, request, queryset):
+
+    def queryset(self, request, queryset):
         if self.value():
             return queryset.filter(curso__id__exact=self.value())
         else:
             return queryset
 
+#
+#
 class AreaAdmin(admin.ModelAdmin):
 	class Meta:
 		model = Area
 
 class ClaseAdmin(admin.ModelAdmin):
-	list_filter = (CursoFilter,)
+	
+	list_filter = (CountryFilter,)
+
 	ordering = ['nombre', 'sorting', 'curso', ]
 	list_editable = ['sorting', ] 
 	list_display = ['nombre', 'sorting', 'curso', ]
-	readonly_fields = ('clase_slug', 'sorting')
-	
+	readonly_fields = ['clase_slug', 'sorting', ]
+
 	class Meta:
 		model = Clase
 
