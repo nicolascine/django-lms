542032266bf5ac6972d474f90b10f6cc080bbc61
nicolas silva <sovietfrance@gmail.com>
2014-10-18 00:01:05 -0300

    clean code

--- src/cursos/views.py
+++ src/cursos/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render, render_to_response, RequestContext, redirec
 from cursos.models import Unidad, Curso, Clase
 from django.db import models
 
-# Create your views here.
-
 def home(request):
 	return render_to_response("home.html", 
 							  locals(), 
@@ -30,7 +28,6 @@ def clasedetalle(request, slug, clase_slug):
 		#return HttpResponse("Necesitas Loguearte!")
 		 return redirect('/cuentas/entrar/')
 	else:
-
 		#listado_clases = Clase.objects.filter(curso=curso_id)
 		#curso = Curso.objects.get(slug=slug)
 		clase = Clase.objects.get(clase_slug=clase_slug)
@@ -52,8 +49,6 @@ def clasedetalle(request, slug, clase_slug):
 		siguiente = clase_siguiente(clase.sorting)
 		anterior = clase_anterior(clase.sorting)
 
-
-
 	return render_to_response("clase_detalle.html", 
 								  locals(), 
 								  context_instance = RequestContext(request))
@@ -62,4 +57,4 @@ def suscribirme(request, curso_id):
 	curso = Curso.objects.get(id=curso_id)
 	return render_to_response("suscribirme.html", 
 							  locals(), 
-							  context_instance = RequestContext(request))		
+							  context_instance = RequestContext(request))	
