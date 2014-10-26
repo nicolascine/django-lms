4f1f82ab6caf86f20a1257a097f7126400233787
nicolas silva <sovietfrance@gmail.com>
2014-10-26 16:56:50 -0300

    clean solution validation inline fields: clases

--- src/cursos/admin.py
+++ src/cursos/admin.py
@@ -40,79 +40,25 @@ class CountryFilter(SimpleListFilter):
         else:
             return queryset
 
-#
-#
 class AreaAdmin(admin.ModelAdmin):
 	class Meta:
 		model = Area
 
-
-## VARIABLES GLOBALES VALIDACION Posicion de clases
-global arreglo
-arreglo = []
-
-def validaPosicion(n):
-	#print "count vale:", count
-	obj = Clase.objects.filter(curso_id=n)
-	total = Clase.objects.filter(curso_id=n).count()
-	#print "el total de clases es: ", total
-	for p in obj:
-		arreglo.append(p.sorting)
-	#return arreglo
 
 class ListaClaseForm(forms.ModelForm):
 
   	class Meta:
 		model = Clase
 	
-	
-
- 	def clean_sorting(self):
+	def clean_sorting(self):
  		
-
-		"""
-
-		if Clase.objects.filter(curso_id=self.instance.curso.id, sorting=(self.cleaned_data['sorting'])):
-			print('existe un registro igual:', str(self.cleaned_data['sorting']))
- 		---->
-		def funciona(n):
-			if count + 1 == 2:
-				print "count +1 == 2"
-			arreglo.append(n)
-
+		#print self.instance.id, "---", self.instance.sorting
 		k = Clase.objects.filter(curso_id=self.instance.curso.id)
-		
-		for r in k:
-			funciona(r.sorting)
-			#print self.cleaned_data['sorting']
-		
-		cuenta = Clase.objects.filter(curso_id=self.instance.curso.id, sorting=(self.cleaned_data['sorting'])).count()
-		todos = Clase.objects.filter(curso_id=self.instance.curso.id, sorting=(self.cleaned_data['sorting']))
-		for registro in todos:
-			if cuenta > 1 and self.cleaned_data['sorting'] == registro.sorting:
-				raise forms.ValidationError("Ya existe este registro")
-		"""
-		"""
-		for solo in todos:
-			if cuenta>1 and self.cleaned_data['sorting']==solo.sorting:
-				raise forms.ValidationError("este error si funciona :) ")
-
-		for t in p:
-			#ahora = timezone.now()
-			if (self.cleaned_data['sorting'] == t.sorting and t.updated > ahora):
-				raise forms.ValidationError("esto ya existe")
-		"""		
+		for p in k:
+			if p.sorting == self.cleaned_data['sorting'] and self.instance.id != p.id:
+				raise forms.ValidationError("Otra clase utiliza la posicion %s" % self.cleaned_data['sorting'])
 		return self.cleaned_data['sorting']
 
-		#arreglo = Clase.objects.filter(curso_id=self.instance.curso.id)
-		#print arreglo
-
-		"""
-		if Clase.objects.filter(curso_id=self.instance.curso.id, sorting=self.cleaned_data['sorting']):
-			print Clase.objects.filter(curso_id=self.instance.curso.id, sorting=self.cleaned_data['sorting'])
-			raise forms.ValidationError("esto ya existe")
-		return self.cleaned_data['sorting']
-		"""
 class ClaseAdmin(admin.ModelAdmin):
 
 	form = ListaClaseForm
@@ -125,39 +71,12 @@ class ClaseAdmin(admin.ModelAdmin):
 
 	def get_changelist_formset(self, request, **kwargs):
 	 		
-	 		if request.GET and request.GET['curso']:
-	 			del arreglo[:]
-	 			#print request.GET['curso']
-				validaPosicion(request.GET['curso'])
-	 			print "arreglo en el GET: ", arreglo
-	 		"""
-			if request.POST:
-				try:
-					del arreglo[:]
-					print "cambio el curso ------->"
-					print "el valor del arreglo AHORA ESSSSS: ", arreglo
-				except Exception, e:
-					pass
-
-				for x in xrange(0,int(request.POST['form-TOTAL_FORMS'])):
-					validaPosicion(request.POST['form-'+str(x)+'-sorting'])
-					pass
-			"""
-				#print "existe un total de:", request.POST['form-TOTAL_FORMS'], "registros"
-				#print request.POST
-			"""
-	 		try:
-	 			curso = request.GET['curso']
-	 			validaPosicion(curso)
-	 		except Exception, e:
-	 			pass
-			"""
 			defaults = {
 	            "formfield_callback": partial(super(ClaseAdmin, self).formfield_for_dbfield, request=request),
 	            "form": ListaClaseForm,
 	        }
 			defaults.update(kwargs)
-	 		#print "el ID del curso es --->>>>", self.curso.id
+
 			return modelformset_factory(Clase,
 	                                    extra=0,
 	                                    fields=self.list_editable, **defaults)
