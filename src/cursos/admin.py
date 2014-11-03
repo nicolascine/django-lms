850bb18b0424a96e5cfd225e01c2d929547c7304
nicolas silva <sovietfrance@gmail.com>
2014-11-03 20:52:28 -0300

    remove example filter

--- src/cursos/admin.py
+++ src/cursos/admin.py
@@ -7,37 +7,19 @@ from django.forms.models import (modelform_factory, modelformset_factory,
 from django.contrib.admin import ModelAdmin, SimpleListFilter
 from .models import Area, Curso, Clase, Unidad
 
-class CursoFilter(SimpleListFilter):
-    title = 'curso' # or use _('curso') for translated title
-    parameter_name = 'curso'
-    
-    def lookups(self, request, model_admin):
-        cuursos = set([c.curso for c in model_admin.model.objects.all()])
-        return [(c.id, c.nombre) for c in cuursos]
-	
+class ClaseFilter(SimpleListFilter):
+	title = 'curso' # or use _('curso') for translated title
+	parameter_name = 'curso'
+
+	def lookups(self, request, model_admin):
+		filtrocurso = set([c.curso for c in model_admin.model.objects.all()])
+		return [(c.id, c.nombre) for c in filtrocurso]
+
 	def queryset(self, request, queryset):
 		if self.value():
 			return queryset.filter(curso__id__exact=self.value())
 		else:
 			return queryset
-
-
-# admin.py
-class CountryFilter(SimpleListFilter):
-    title = 'curso' # or use _('country') for translated title
-    parameter_name = 'curso'
-
-    def lookups(self, request, model_admin):
-        countries = set([c.curso for c in model_admin.model.objects.all()])
-        return [(c.id, c.nombre) for c in countries]
-        # You can also use hardcoded model name like "Country" instead of 
-        # "model_admin.model" if this is not direct foreign key filter
-
-    def queryset(self, request, queryset):
-        if self.value():
-            return queryset.filter(curso__id__exact=self.value())
-        else:
-            return queryset
 
 class AreaAdmin(admin.ModelAdmin):
 	class Meta:
@@ -85,7 +67,7 @@ class ClaseAdmin(admin.ModelAdmin):
 		return super(ClaseAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
 
-	list_filter = (CountryFilter,)
+	list_filter = (ClaseFilter,)
 	ordering = ['sorting', ]
 	list_editable = ['sorting', ] 
 	list_display = ['nombre', 'curso', 'unidad', 'sorting', ]
