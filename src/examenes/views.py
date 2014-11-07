c0db6af6e1d175dd65e08b5a334f6a7225d4e385
nicolas silva <sovietfrance@gmail.com>
2014-11-06 22:33:41 -0300

    add auth login confirmation

--- src/examenes/views.py
+++ src/examenes/views.py
@@ -16,7 +16,6 @@ def examandetalle(request, slug, examen_slug):
 		for K in preguntaDelExamen:
 			arregloPreguntas.append(K.pregunta_id)
 
-
 		listado_preguntas = Pregunta.objects.filter(id__in = arregloPreguntas)
 		
 	return render_to_response("examen_detalle.html", 
@@ -24,11 +23,39 @@ def examandetalle(request, slug, examen_slug):
 							  context_instance = RequestContext(request))
 
 def preguntadetalle(request, slug, examen_slug, pregunta_id):
-	curso = Curso.objects.get(slug=slug)
-	examen = Examen.objects.get(examen_slug=examen_slug)
-	pregunta = Pregunta.objects.get(id=pregunta_id)
+	if not request.user.is_authenticated():
+		#return HttpResponse("Necesitas Loguearte!")
+		 return redirect('/cuentas/entrar/')
+	else:
+		curso = Curso.objects.get(slug=slug)
+		examen = Examen.objects.get(examen_slug=examen_slug)
+		pregunta = Pregunta.objects.get(id=pregunta_id)
+		pregenExam = PreguntaEnExamen.objects.get(pregunta_id = pregunta.id)
+		
+		def pregunta_siguiente(numero):
+			try:
+				obj = PreguntaEnExamen.objects.get(examen_id = examen.id, sorting=(numero+1))
+				next = Pregunta.objects.get(id = obj.id)
+				return next
+			except PreguntaEnExamen.DoesNotExist:
+				return None
+		def pregunta_anterior(numero):
+			try:
+				obj = PreguntaEnExamen.objects.get(examen_id = examen.id, sorting=(numero-1))
+				prev = Pregunta.objects.get(id = obj.id)
+				return prev
+			except PreguntaEnExamen.DoesNotExist:
+				return None
+		
+		anterior = pregunta_anterior(pregenExam.sorting)
+		siguiente = pregunta_siguiente(pregenExam.sorting)
+		
+		print "PREGUNTA:", pregunta
+		print "ANTERIOR:", anterior
+		print "SIGUIENTE:", siguiente
 
-	listado_respuestas = Respuesta.objects.filter(pregunta_id=pregunta.id)
-	return render_to_response("pregunta_detalle.html", 
-							  locals(),
-							  context_instance = RequestContext(request))
+		listado_respuestas = Respuesta.objects.filter(pregunta_id=pregunta.id)
+
+		return render_to_response("pregunta_detalle.html", 
+								  locals(),
+								  context_instance = RequestContext(request))
