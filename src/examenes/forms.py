903cee0e2dfe0bf6dcb166362b7bc39b78fce0a5
nicolas silva <sovietfrance@gmail.com>
2014-12-02 15:21:47 -0300

    change name Form (duplicate name: form/model)

--- src/examenes/forms.py
+++ src/examenes/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from examenes.models import *
 
-class RespuestasDelUsuario(forms.ModelForm):
+class UsuarioFormuRespuestas(forms.ModelForm):
 	
 
 	def clean(self):
@@ -9,7 +9,7 @@ class RespuestasDelUsuario(forms.ModelForm):
 		cleaned_data = self.cleaned_data
 		respuesta = cleaned_data.get("respuesta")
 		if respuesta == None:
-			raise forms.ValidationError("Debes ingresar una respuesta")
+			raise forms.ValidationError("Debes seleccionar una respuesta")
 		return cleaned_data
 
 	class Meta:
